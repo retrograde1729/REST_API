cc109c0f04ee83e91bd335c22973a688e0c8b03f
Bishnu <21ucs249@nmiit.ac.in>
2025-02-22 20:59:27 +0530

    fun time

---
+++ app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", callback)
+	router.Run(":8080")
+}
+
+func callback(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
